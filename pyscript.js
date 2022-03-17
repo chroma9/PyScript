0b5913031bc3c65f42496497a5cb11ba9056b9fd
Chroma9 <64189171+chroma9@users.noreply.github.com>
2022-03-17 03:35:09 -0400

    Added the first batch of String extensions.

--- pyscript.js
+++ pyscript.js
@@ -4,3 +4,26 @@ class PyScriptError extends Error {
         this.name = ('PyScriptError');
     }
 }
+/**
+ * Convert String to Upper Case
+ * @returns String
+ */
+String.prototype.upper = function() {
+    return this.toUpperCase();
+}
+/**
+ * Converts String to Lower Case
+ * @returns String
+ */
+String.prototype.lower = function() {
+    return this.toLowerCase();
+}
+/**
+ * Capitalize the First Character in the String
+ * @returns String
+ */
+String.prototype.capitalize = function() {
+    var a = this.charAt(0).toUpperCase();
+    var b = this.slice(1).toLowerCase();
+    return (a + b);
+}
